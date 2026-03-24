baef9aabac6e484a8a07463ddb65c7f72ff886b1
RuvayRamdhin <ST10522098@myemeris.edu.za>
2026-03-24 13:59:47 +0200

    Initializing values

--- app/src/main/java/com/example/sparkapp/MainActivity.kt
+++ app/src/main/java/com/example/sparkapp/MainActivity.kt
@@ -25,6 +25,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //Initializing values (TypeCasting)
+        tvTitle = findViewById(R.id.tvTitle)
+        tvResult = findViewById(R.id.tvResult)
+        edtSparkOption = findViewById(R.id.edtSparkOption)
+        btnCheckSpark = findViewById(R.id.btnCheckSpark)
+        btnReset = findViewById(R.id.btnReset)
+        btnOptions = findViewById(R.id.btnOptions)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
