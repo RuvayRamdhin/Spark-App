fae36e05697f6a428e32662434a6939376b65b7f
RuvayRamdhin <ST10522098@myemeris.edu.za>
2026-03-24 13:51:41 +0200

    Declarations of variables

--- app/src/main/java/com/example/sparkapp/MainActivity.kt
+++ app/src/main/java/com/example/sparkapp/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.sparkapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.DialogTitle
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.TextViewCompat
 
 class MainActivity : AppCompatActivity() {
+
+    //Declarations of variables
+    private lateinit var tvTitle: TextView
+    private lateinit var tvResult: TextView
+    private lateinit var edtSparkOption: EditText
+    private lateinit var btnCheckSpark: Button
+    private lateinit var btnReset: Button
+    private lateinit var btnOptions: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
